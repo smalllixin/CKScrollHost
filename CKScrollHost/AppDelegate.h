71aa5b32acbeafc810e6629f9cb09b5495124b55
smalllixin <smalllixin@gmail.com>
2015-10-27 10:23:54 -0700

    Initial Commit

---
+++ CKScrollHost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CKScrollHost
+//
+//  Created by lixin on 10/27/15.
+//  Copyright © 2015 lixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CKScrollHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CKScrollHost
+//
+//  Created by lixin on 10/27/15.
+//  Copyright © 2015 lixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
